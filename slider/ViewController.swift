3a0593ac1ba3a63adcdef25258d120c4adf7d7e3
D7703_07 <d7703_07@lulzsec.local>
2019-05-02 12:46:40 +0900

    commit

--- slider/ViewController.swift
+++ slider/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        lbl.text = String(Int(slider.value))
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        
+        cell.textLabel?.text = String(Int(slider.value) + indexPath.row)
+        
+        return cell
+    }
+    
 }
 
